eb3171bab691fcb30e2abc9b4e5f1e37b5f9f041
Aman Singh <amansingh9097@gmail.com>
2019-05-08 00:45:36 +0530

    added aaj_kya_kiya text file

--- mailer/notify.py
+++ mailer/notify.py
@@ -1,8 +1,9 @@
 # notify me to update my today's tasks in mail_body.txt file
 # for the mailer to send the maile to Meg
 
+# using win10toast to show notification at the bottom-right of the screen
 from win10toast import ToastNotifier
 
 toaster = ToastNotifier()
 
-toaster.show_toast("mailer asks:", "AAJ KYA UKHADA BETA??\n\nTASKS UPDATE KAR body_mail.txt ME...")
+toaster.show_toast("AAJ KYA UKHAD LIYA??", "Update your today's tasks in 'aaj_kya_kiya.txt' before 5 pm IST.")
